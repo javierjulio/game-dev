04552c30a953a581164a7668fb6a3d00dec0d75d
Javier Julio <jjfutbol@gmail.com>
2022-07-23 19:01:07 -0400

    Update vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
+
+const ROOT = "src"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, ROOT, 'index.html'),
+        bitecs: resolve(__dirname, ROOT, 'bitecs-circle-collision.html'),
+        ecsy: resolve(__dirname, ROOT, 'ecsy-example.html')
+      }
+    }
+  },
   server: {
     // host: true, // Enable to expose host for running app on phone
     open: true
   },
   base: '/game-dev/', // use repo name https://vitejs.dev/guide/static-deploy.html#github-pages
-  root: 'src'
+  root: ROOT
 })
